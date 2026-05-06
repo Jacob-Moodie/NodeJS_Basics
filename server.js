5245b6dab00ce5bf2e0aa550bceccbdbc000ef34
Jacob-Moodie <jtmoodie01@students.ntc.edu>
2026-05-06 06:21:01 -0500

    aa

--- server.js
+++ server.js
@@ -1,25 +1,17 @@
 const fs = require("fs");
 const { createServer } = require("http");
-const { 
-createWriteStream,
-createReadStream
-} = require("util");
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 createServer((req, res) => {
   try {
-    res.writeHead(200, {
-        "Content-Type": "text/html"
-    })
-    res.end( 
-       "Welcome to the server!"
-    )
-    } catch (error) {
-        console.error(error);
-        res.writeHead(500, {
-            "Content-Type": "text/plain"
-        });
-        res.end("Server Error")
-    }
-}).listen(port, () => console.log("Server 3000 is running!"));
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("Welcome to the server!");
+  } catch (error) {
+    console.error(error);
+    res.writeHead(500, { "Content-Type": "text/plain" });
+    res.end("Server Error");
+  }
+}).listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
